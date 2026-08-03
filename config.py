d191786f13368859c4e17eb987bac8a119ecc0c6
Darkill97 <115864720+Darkill97@users.noreply.github.com>
2026-08-03 14:32:17 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+import os
 
+# Token del bot (Render lo leerá de las variables de entorno)
+TOKEN = os.getenv("TOKEN")
+
+# Base de datos PostgreSQL (Render la leerá de las variables de entorno)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Configuración por defecto
+HORAS_TURNO = 8
+
+# Tarifas por defecto (se podrán cambiar desde Telegram)
+TARIFA_NORMAL = 721.17
+TARIFA_TIME_HALF = TARIFA_NORMAL * 1.5
+TARIFA_BH = TARIFA_NORMAL * 0.5
+
+# API OCR gratuita
+OCR_API_KEY = "helloworld"
